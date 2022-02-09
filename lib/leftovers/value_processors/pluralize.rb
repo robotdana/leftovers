d190c469c08dd3c7bfe75e5bcafc36a33e19f28f
Dana Sherson <robot@dana.sh>
2022-02-09 17:53:23 +1300

    Support active_support 7
    Use the documented way of requiring just the core extensions

--- lib/leftovers/value_processors/pluralize.rb
+++ lib/leftovers/value_processors/pluralize.rb
@@ -16,7 +16,7 @@ module Leftovers
       rescue NoMethodError
         Leftovers.error <<~MESSAGE
           Tried using the String#pluralize method, but the activesupport gem was not available and/or not required
-          `gem install activesupport`, and/or add `requires: 'active_support/core_ext/string'` to your .leftovers.yml
+          `gem install activesupport`, and/or add `requires: ['active_support', 'active_support/core_ext/string']` to your .leftovers.yml
         MESSAGE
       end
     end
