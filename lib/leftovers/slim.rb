f565a1cc8c39514f36a824e236399c0cc6d6b148
Shane Becker <veganstraightedge@gmail.com>
2022-02-08 20:50:31 -0800

    Add Leftovers::Slim module

---
+++ lib/leftovers/slim.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Leftovers
+  module Slim
+    module_function
+
+    def precompile(file, name)
+      return '' unless Leftovers.try_require('slim', message: <<~MESSAGE) # rubocop:disable Layout/EmptyLineAfterGuardClause
+        Skipped parsing #{name.relative_path}, because the slim gem was not available
+        `gem install slim`
+      MESSAGE
+
+      begin
+        ::Slim::Engine.new(file).precompiled
+      rescue ::Slim::SyntaxError => e
+        Leftovers.warn "#{e.class}: #{e.message} #{name.relative_path}:#{e.line}"
+        ''
+      end
+    end
+  end
+end
